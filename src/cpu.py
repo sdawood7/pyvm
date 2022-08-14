2db3bb8252443e2d6bb1eee723ede0aaabcca46b
Shehryar Dawood <trident0011@gmail.com>
2022-08-13 20:12:33 -0500

    Add link register and accumulator

--- src/cpu.py
+++ src/cpu.py
@@ -6,7 +6,7 @@ class CPU:
         self.memory = Memory(memory_size)
 
         self.registerNames = [
-            'ip',
+            'ip', 'lr', 'ac',
             'r1', 'r2', 'r3', 'r4',
             'r5', 'r6', 'r7', 'r8',
             'sp', 'fp',
