26047aac5bef44f1505a19eb98bee69d0c01e189
Shehryar Dawood <trident0011@gmail.com>
2022-08-15 20:09:38 -0500

    Convert switch case to if elif

--- src/cpu.py
+++ src/cpu.py
@@ -89,259 +89,258 @@ class CPU:
         return instruction
 
     def execute(self, instruction) -> int:
-        match instruction:
-            # Register/Memory manipulation
-            case Instruction.LW:
-                rd = self.fetch()
-                address = self.fetchWord()
+        # Register/Memory manipulation
+        if instruction == Instruction.LW:
+            rd = self.fetch()
+            address = self.fetchWord()
 
-                word_from_memory = self.readFromMemory(address)
+            word_from_memory = self.readFromMemory(address)
 
-                self.setRegisterValue(rd, word_from_memory)
-                return 1
-            case Instruction.LWR:
-                rd = self.fetch()
-                rs = self.fetch()
+            self.setRegisterValue(rd, word_from_memory)
+            return 1
+        elif instruction == Instruction.LWR:
+            rd = self.fetch()
+            rs = self.fetch()
 
-                address = self.getRegisterValue(rs)
-                word_from_memory = self.readFromMemory(address)
+            address = self.getRegisterValue(rs)
+            word_from_memory = self.readFromMemory(address)
 
-                self.setRegisterValue(rd, word_from_memory)
-                return 1
-            case Instruction.SW:
-                rs = self.fetch()
-                address = self.fetchWord()
+            self.setRegisterValue(rd, word_from_memory)
+            return 1
+        elif instruction == Instruction.SW:
+            rs = self.fetch()
+            address = self.fetchWord()
 
-                word_to_memory = self.getRegisterValue(rs)
+            word_to_memory = self.getRegisterValue(rs)
 
-                self.writeToMemory(address, word_to_memory)
-                return 1
-            case Instruction.SWR:
-                rs = self.fetch()
-                rd = self.fetch()
+            self.writeToMemory(address, word_to_memory)
+            return 1
+        elif instruction == Instruction.SWR:
+            rs = self.fetch()
+            rd = self.fetch()
 
-                address = self.getRegisterValue(rd)
-                word_to_memory = self.getRegisterValue(rs)
+            address = self.getRegisterValue(rd)
+            word_to_memory = self.getRegisterValue(rs)
 
-                self.writeToMemory(address, word_to_memory)
-                return 1
-            case Instruction.MOV:
-                rd = self.fetch()
-                immediate = self.fetchWord()
+            self.writeToMemory(address, word_to_memory)
+            return 1
+        elif instruction == Instruction.MOV:
+            rd = self.fetch()
+            immediate = self.fetchWord()
 
-                self.setRegisterValue(rd, immediate)
-                return 1
-            case Instruction.SWP:
-                r1 = self.fetch()
-                r2 = self.fetch()
+            self.setRegisterValue(rd, immediate)
+            return 1
+        elif instruction == Instruction.SWP:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                self.push(self.getRegisterValue(r1))
-                self.push(self.getRegisterValue(r2))
+            self.push(self.getRegisterValue(r1))
+            self.push(self.getRegisterValue(r2))
 
-                self.setRegisterValue(r1, self.pop())
-                self.setRegisterValue(r2, self.pop())
+            self.setRegisterValue(r1, self.pop())
+            self.setRegisterValue(r2, self.pop())
 
-                return 1
+            return 1
 
-            # Stack Operations
-            case Instruction.PSH:
-                rs = self.fetch()
-                value = self.getRegisterValue(rs)
+        # Stack Operations
+        elif instruction == Instruction.PSH:
+            rs = self.fetch()
+            value = self.getRegisterValue(rs)
 
-                self.push(value)
+            self.push(value)
 
-                return 1
-            case Instruction.PSHI:
-                value = self.fetchWord()
+            return 1
+        elif instruction == Instruction.PSHI:
+            value = self.fetchWord()
 
-                self.push(value)
+            self.push(value)
 
-                return 1
-            case Instruction.POP:
-                rd = self.fetch()
+            return 1
+        elif instruction == Instruction.POP:
+            rd = self.fetch()
 
-                value = self.pop()
+            value = self.pop()
 
-                self.setRegisterValue(rd, value)
-                
-                return 1
-            case Instruction.JAL:
-                return 1
-            case Instruction.RET:
-                return 1
-
-            # Arithmetic
-            case Instruction.ADD:
-                r1 = self.fetch()
-                r2 = self.fetch()
-
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
-
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val + r2Val
-
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.ADDI:
-                return 1
-            case Instruction.SUB:
-                r1 = self.fetch()
-                r2 = self.fetch()
-
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
-
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val - r2Val
-
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.SUBI:
-                return 1
-            case Instruction.MULT:
-                r1 = self.fetch()
-                r2 = self.fetch()
-
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
-
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val * r2Val
-
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.MULTI:
-                return 1
-            case Instruction.DIV:
-                r1 = self.fetch()
-                r2 = self.fetch()
-
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
-
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val / r2Val
-
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.DIVI:
-                return 1
-            case Instruction.MOD:
-                r1 = self.fetch()
-                r2 = self.fetch()
-
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
-
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val % r2Val
-
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.MODI:
-                return 1
+            self.setRegisterValue(rd, value)
             
-            # Logical Operands
-            case Instruction.AND:
-                r1 = self.fetch()
-                r2 = self.fetch()
+            return 1
+        elif instruction == Instruction.JAL:
+            return 1
+        elif instruction == Instruction.RET:
+            return 1
 
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
+        # Arithmetic
+        elif instruction == Instruction.ADD:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val & r2Val
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
 
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.ANDI:
-                return 1
-            case Instruction.OR:
-                r1 = self.fetch()
-                r2 = self.fetch()
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val + r2Val
 
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.ADDI:
+            return 1
+        elif instruction == Instruction.SUB:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val | r2Val
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
 
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.ORI:
-                return 1
-            case Instruction.XOR:
-                r1 = self.fetch()
-                r2 = self.fetch()
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val - r2Val
 
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.SUBI:
+            return 1
+        elif instruction == Instruction.MULT:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val ^ r2Val
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
 
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.XORI:
-                return 1
-            case Instruction.LSHFT:
-                r1 = self.fetch()
-                r2 = self.fetch()
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val * r2Val
 
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.MULTI:
+            return 1
+        elif instruction == Instruction.DIV:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val << r2Val
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
 
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.LSHFTI:
-                return 1
-            case Instruction.RSHFT:
-                r1 = self.fetch()
-                r2 = self.fetch()
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val / r2Val
 
-                r1Val = self.getRegisterValue(r1)
-                r2Val = self.getRegisterValue(r2)
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.DIVI:
+            return 1
+        elif instruction == Instruction.MOD:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                ac_reg = self.getRegisterIndex('ac')
-                val = r1Val >> r2Val
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
 
-                self.setRegisterValue(ac_reg, val)
-                return 1
-            case Instruction.RSHFTI:
-                return 1
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val % r2Val
 
-            # Instruction Pointer Manipulation
-            case Instruction.JR:
-                rs = self.fetch()
-                address = self.getRegisterValue(rs)
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.MODI:
+            return 1
+        
+        # Logical Operands
+        elif instruction == Instruction.AND:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                self.jump(address)
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
 
-                return 1
-            case Instruction.JI:
-                address = self.fetchWord()
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val & r2Val
 
-                self.jump(address)
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.ANDI:
+            return 1
+        elif instruction == Instruction.OR:
+            r1 = self.fetch()
+            r2 = self.fetch()
 
-                return 1
-            case Instruction.BEQ:
-                return 1
-            case Instruction.BNE:
-                return 1
-            case Instruction.BLT:
-                return 1
-            case Instruction.BLE:
-                return 1
-            case Instruction.BGT:
-                return 1
-            case Instruction.BGE:
-                return 1
-            case Instruction.HLT:
-                return 0
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
+
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val | r2Val
+
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.ORI:
+            return 1
+        elif instruction == Instruction.XOR:
+            r1 = self.fetch()
+            r2 = self.fetch()
+
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
+
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val ^ r2Val
+
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.XORI:
+            return 1
+        elif instruction == Instruction.LSHFT:
+            r1 = self.fetch()
+            r2 = self.fetch()
+
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
+
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val << r2Val
+
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.LSHFTI:
+            return 1
+        elif instruction == Instruction.RSHFT:
+            r1 = self.fetch()
+            r2 = self.fetch()
+
+            r1Val = self.getRegisterValue(r1)
+            r2Val = self.getRegisterValue(r2)
+
+            ac_reg = self.getRegisterIndex('ac')
+            val = r1Val >> r2Val
+
+            self.setRegisterValue(ac_reg, val)
+            return 1
+        elif instruction == Instruction.RSHFTI:
+            return 1
+
+        # Instruction Pointer Manipulation
+        elif instruction == Instruction.JR:
+            rs = self.fetch()
+            address = self.getRegisterValue(rs)
+
+            self.jump(address)
+
+            return 1
+        elif instruction == Instruction.JI:
+            address = self.fetchWord()
+
+            self.jump(address)
+
+            return 1
+        elif instruction == Instruction.BEQ:
+            return 1
+        elif instruction == Instruction.BNE:
+            return 1
+        elif instruction == Instruction.BLT:
+            return 1
+        elif instruction == Instruction.BLE:
+            return 1
+        elif instruction == Instruction.BGT:
+            return 1
+        elif instruction == Instruction.BGE:
+            return 1
+        elif instruction == Instruction.HLT:
+            return 0
 
 
     def step(self) -> int:
