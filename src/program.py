4f9fc69271221b38670f95c18446e05eff63d5e2
Shehryar Dawood <trident0011@gmail.com>
2022-08-30 21:35:49 -0500

    Create program class to easily add new instructions

---
+++ src/program.py
@@ -0,0 +1,20 @@
+class Program:
+    def __init__(self) -> None:
+        self.byte_count = 0
+        self.functions = {}
+
+    def instruction(self, instruction=None, arg1=None, arg2=None, func="main") -> None:
+        if func not in self.functions:
+            self.functions[func] = []
+        if instruction != None:
+            self.functions[func].append(instruction)
+            self.byte_count += 1
+        else:
+            return
+        if arg1 != None:
+            self.functions[func].append(arg1)
+            self.byte_count += 1
+
+        if arg1 != None:
+            self.functions[func].append(arg2)
+            self.byte_count += 1
