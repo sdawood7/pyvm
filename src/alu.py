ce7713f0a636b35b99e89e321695c46442636956
Shehryar Dawood <trident0011@gmail.com>
2022-09-02 14:37:22 -0500

    rearrange alu

--- src/alu.py
+++ src/alu.py
@@ -8,10 +8,30 @@ class ALU:
         self.zero = True
         self.result = 0
 
+    def convert2scompl(self, n):
+        n &= 0xFFFF # Convert to 16 bit
+        if n >= 0x8000 and n <= 0xFFFF:
+            return ~n + 1
+        return n
+
+    def setFlags(self):
+        if self.result > 0xFFFF:
+            self.overflow = True
+
+        if self.result == 0:
+            self.zero = True
+            self.negative = False
+        elif self.result < 0:
+            self.zero = False
+            self.negative = True
+        else:
+            self.zero = False
+            self.negative = False
+
     def compute(self, op, val1, val2=0) -> None: # Return accumulator value and flags
         self.reset()
-        val1 = convert2scompl(val1)
-        val2 = convert2scompl(val2)
+        val1 = self.convert2scompl(val1)
+        val2 = self.convert2scompl(val2)
 
         if op == "+":
             self.result = val1 + val2
@@ -22,7 +42,7 @@ class ALU:
         elif op == "/":
             if val2 == 0:
                 raise Exception("Divide by 0 Error")
-            self.result = val1 / val2
+            self.result = int(val1 / val2)
         elif op == "%":
             if val2 == 0:
                 raise Exception("Modulo by 0 Error")
@@ -42,24 +62,5 @@ class ALU:
         else:
             raise Exception("Unknown operator \"{}\" given".format(op))
 
-        setFlags()
+        self.setFlags()
 
-        def convert2scompl(n):
-            n &= 0xFFFF # Convert to 16 bit
-            if n >= 0x8000 and n <= 0xFFFF:
-                return ~n + 1
-            return n
-
-        def setFlags():
-            if self.result > 0xFFFF:
-                self.overflow = True
-
-            if self.result == 0:
-                self.zero = True
-                self.negative = False
-            elif self.result < 0:
-                self.zero = False
-                self.negative = True
-            else:
-                self.zero = False
-                self.negative = False
