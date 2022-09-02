52ff1867c07ccc7fec8bcd4cbaebe8e4fa449f88
Shehryar Dawood <trident0011@gmail.com>
2022-09-02 14:59:33 -0500

    Add label jumps to allow dynamic function calls

--- src/program.py
+++ src/program.py
@@ -1,15 +1,26 @@
+from instruction import Instruction
+
+
 class Program:
     def __init__(self) -> None:
         self.byte_count = 0
         self.functions = {}
+        self.labels = {}
 
-    def instruction(self, instruction=None, arg1=None, arg2=None, func="main") -> None:
+    def instruction(self, instruction=None, arg1=None, arg2=None, func="main", label=None) -> None:
         if func not in self.functions:
             self.functions[func] = []
+            self.labels[func] = self.byte_count
 
         if instruction != None:
             self.functions[func].append(instruction)
             self.byte_count += 1
+            if label != None and arg1 == None and arg2 == None:
+                if label not in self.labels:
+                    raise Exception("Label \"{}\" not yet declared".format(label))
+                jump_address = self.labels[label]
+                arg1 = (jump_address & 0xff00) >> 8
+                arg2 = (jump_address & 0x00ff)
         else:
             return
 
